533305b839e17e3ea3bfb327ad473a0c92cf0db3
serranoconstruction <rod5547@gmail.com>
2025-01-13 09:54:52 -0800

    touched some things up

--- src/app/gallery/_components/gallery-content.tsx
+++ src/app/gallery/_components/gallery-content.tsx
@@ -7,9 +7,6 @@ import { api } from "~/trpc/react";
 export const GalleryContent: React.FC = () => {
   const query = api.gallery.get.useQuery();
 
-  // Define a set of heights to cycle through
-  const heights = ["20rem", "25rem", "30rem"];
-
   if (query.isPending || query.isLoading) {
     return (
       <div className="flex h-full items-center justify-center">
@@ -24,12 +21,12 @@ export const GalleryContent: React.FC = () => {
 
   return (
     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
-      {query.data.map((image) => (
+      {query.data.map((image, i) => (
         <img
           key={image.id}
           src={image.url}
           alt={image.fileName}
-          className="max-h-[30rem] w-full rounded-lg object-cover transition-transform duration-500"
+          className="h-[40rem] w-full object-cover"
         />
       ))}
     </div>
